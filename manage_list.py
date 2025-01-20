f16824dda55a2b252c65fb5af97e04c801b3a7f1
rako <rakoinfotech@gmail.com>
2025-01-21 01:16:25 +0900

    入力チェックを追加

--- manage_list.py
+++ manage_list.py
@@ -1,8 +1,28 @@
 ### チャンネルを管理する
 
+# 入力チェック
+
+def validate_url_input(input_data):
+    if 'https://www.youtube.com/channel/' in input_data or 'https://www.youtube.com/@' in input_data or 'https://www.youtube.com/c/' in input_data:
+        return True
+    
+    if input_data:
+        return True
+    
+    return False
+
+
+# チャンネルIDが存在しているかチェック
+
+def check_channel_id():
+    pass
+
+
 # チャンネルを追加する
 
 def add():
+    if validate_url_input() == False:
+        pass
     pass
 
 
