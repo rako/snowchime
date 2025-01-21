0b64b6cdc4e6cb07efda33ec082b677cae62d944
rako <rakoinfotech@gmail.com>
2025-01-21 16:06:17 +0900

    APIリクエストの大枠を作成

--- search.py
+++ search.py
@@ -1,9 +1,21 @@
 import json, os
+from apiclient.discovery import build
 
-# API検索用キー
 YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY")
+
+
+# APIを作成する
+youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+
+
+# 検索クエリを作成する
+channel_url = "https://www.youtube.com/channel/"
 
 
 # APIに検索をかける
 def search(data):
-    pass
+    response = youtube.search().list(
+        part = "snippet",
+        type = "channel",
+        q = channel_url + data
+    ).execute()
