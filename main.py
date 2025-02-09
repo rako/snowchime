757a15eb37a1895469345559e1bc713aa8c61eb8
rako <69637576+rako@users.noreply.github.com>
2025-02-10 00:03:36 +0900

    GCP Cloud Runにデプロイ

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import requests
-
 import manage_list, search, util
+import functions_framework
 
-def main():
-    pass
+@functions_framework.http
+def main(request):
+    # 購読確認リクエスト処理
+    if "hub.mode" in request.args and request.args["hub.mode"] == "subscribe":
+        if "hub.challenge" in request.args:
+            return Response(status=200, body=request.challenge)
+        else:
+            return Response(status=500)
+    
+    # 実際のリクエスト処理
+    return Response(status=200)
