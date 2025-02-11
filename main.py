4e8a153d2d48ef5a794cc4239fe770b92b79f2bd
rako <69637576+rako@users.noreply.github.com>
2025-02-12 01:49:49 +0900

    トリガー元で分岐させた

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # import manage_list, search, util
+
 import functions_framework
 import os
-import discord
-import requests, json
+import requests
+import xml.etree.ElementTree as ET
 
 discord_token = str(os.environ.get("DISCORD_TOKEN"))
 discord_channel = int(os.environ.get("DISCORD_CHANNEL_ID"))
-client_id = int(os.environ.get("DISCORD_CLIENT_ID"))
-client_secret = str(os.environ.get("DISCORD_CLIENT_SECRET"))
+# client_id = int(os.environ.get("DISCORD_CLIENT_ID"))
+# client_secret = str(os.environ.get("DISCORD_CLIENT_SECRET"))
 
 discord_api_url = f"https://discord.com/api/v10/channels/{discord_channel}/messages"
 
@@ -16,28 +17,58 @@ def main(request):
     # 購読確認リクエスト処理
     if "hub.mode" in request.args and request.args["hub.mode"] == "subscribe":
         challenge = request.args.get("hub.challenge")
+        response = requests.get(request.url)
         if challenge:
-            response = make_response(challenge)
             response.headers["Content-Type"] = "text/plain"
             response.status_code = 200
+            response.content = challenge
             return response
         else:
-            response = make_response("Invalid request", 400)
+            response.status_code = 400
+            response.content = "Invalid request"
             return response
     
     # 実際のリクエスト処理
-    if request.method == "POST":
-        send_message()
-        return "OK", 200
+    if request.method == "POST" and request.host_url == "https://pubsubhubbub.appspot.com":
+        send_message(request)
+        return "sent"
+
+    return "OK"
 
 
-def send_message():
-    main_content = {
-        'content': 'test text'
-    }
+def send_message(request):
     headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bot " + discord_token
+        "Authorization": f"Bot {discord_token}",
+        "Content-Type": "application/json"
+    }
+
+    xmltree = ET.parse(request.get_data())
+    root = xmltree.getroot()
+    entry = root["entry"]
+
+    # フィードの要素の時だけにする
+    if root.tag != "feed":
+        return
+
+    published_date = entry.findtext("published")
+    updated_date = entry.findtext("updated")
+    video_title = entry.findtext("title")
+    video_url = entry.findtext("link/@href")
+    
+    # 動画アップロードしたときだけにする
+    #if published_date != updated_date:
+        #return
+    
+    main_content = {
+        "mention_everyone": True,
+        "content": "新しい動画です！",
+        "channel_id": discord_channel,
+        "embeds": [
+            {
+                "title": video_title,
+                "url": video_url,
+            }
+        ],
     }
 
     response = requests.post(discord_api_url, headers=headers, json=main_content)
