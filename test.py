1a2f15301ea0a6de6eb9dee1319db4bab2c4d217
rako <69637576+rako@users.noreply.github.com>
2025-03-02 22:45:37 +0900

    テストを作成した

---
+++ test.py
@@ -0,0 +1,19 @@
+import requests
+
+url = "https://asia-northeast2-sound-bee-448211-m4.cloudfunctions.net/snowchime"
+
+with open("test_xml.xml", "r", encoding="utf-8") as file:
+    xml_content = file.read()
+
+def test():
+    headers = {
+        "Content-Type": "application/xml"
+    }
+    response = requests.post(url, headers=headers, data=xml_content)
+    
+    # デバッグ用
+    print(response.text)
+    print(f"ステータスコード: {response.status_code}")
+    print(f"レスポンス: {response.text}")
+
+test()
